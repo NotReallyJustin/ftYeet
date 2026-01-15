adf4a20ec1b4a6680a2a0056b8f6db7f757f0292
Justin <JustinChen3946@gmail.com>
2026-01-15 14:44:17 -0500

    Slight modification to bundle script

--- CLI/bundle.ps1
+++ CLI/bundle.ps1
@@ -3,7 +3,7 @@
 # BUT if you're building this yourself, just change the variable below to one of the pkg targets here: https://www.npmjs.com/package/pkg
 
 $TARGET="latest-windows-x64"
-$OUTPUT_PATH="./Executables/ftYeet-windows-x64"
+$OUTPUT_PATH="./Executables/ftYeet-windows-x64.exe"
 
 # Automatically bundles all the .js files and turns it into a standalone executable
 npm install
